fcd7b63544ecf09625468da61908b8c0e7b12965
zeita101 <zeitajb1@gmail.com>
2021-02-08 17:24:23 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ import App from './App';
 import reducer from './store/reducer';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
-const store = createStore(reducer);
+const logAction = store => {
+    return next => {
+        return action => {
+            const result = next(action);
+            console.log(`Caught in the middleware ${JSON.stringify(result)}`);
+            return result;
+        };
+    };
+};
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+
+
 
